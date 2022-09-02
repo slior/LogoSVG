b92ea5ac33f5a43e1c9ef6382a0e56e67fb27e55
slior <lior.schejter@gmail.com>
2022-09-02 10:28:45 +0300

    better encapsulation

--- src/Main.js
+++ src/Main.js
@@ -1,9 +1,29 @@
 
-const LogoProcessor = require("./LogoProcessor.js")
-const DrawingContext = require("./DrawingContext.js")
+const { LogoProcessor } = require("./LogoProcessor.js")
+const { DrawingContext } = require("./DrawingContext.js")
+
+class Painter
+{
+    constructor(processor,context)
+    {
+        this.processor = processor;
+        this.context = context;
+    }
+
+    processStatement(st)
+    {
+        this.processor.processStatement(st,this.context);
+    }
+}
+
+function newPainter(drawingContainer,startingX,startingY,startingAngle)
+{
+    let context = new DrawingContext(startingX,startingY,startingAngle,drawingContainer);
+    let p = new LogoProcessor();
+    return new Painter(p,context);
+}
 
 module.exports = {
-    LogoProcessor,
-    DrawingContext
+    newPainter
 
 }
