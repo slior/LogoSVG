948c3060bec8c3826aff5a45a2490e0ac0d96a7e
slior <lior.schejter@gmail.com>
2022-09-24 22:47:31 +0300

    Reorg: painter in its own file

---
+++ src/Painter.js
@@ -0,0 +1,65 @@
+
+const CURSOR_SIZE = 10;
+
+function radians(deg)
+{
+    return (deg % 360) * Math.PI / 180
+}
+
+
+class Painter
+{
+    constructor(processor,context)
+    {
+        this.processor = processor;
+        this.context = context;
+        this.cursorObj = this.createCursor(this.context);
+        this.moveCursorToContext()
+    }
+
+    createCursor(dc)
+    {
+        let x1 = dc.lastX
+        let y1 = dc.lastY
+        let angleTo1stArm = radians(135);
+        let x2 = x1 + CURSOR_SIZE * Math.cos(angleTo1stArm)
+        let y2 = y1 + CURSOR_SIZE * Math.sin(angleTo1stArm)
+        
+        let angleTo2ndArm = radians(225);
+        let x3 = x1 + CURSOR_SIZE * Math.cos(angleTo2ndArm)
+        let y3 = y1 + CURSOR_SIZE * Math.sin(angleTo2ndArm)
+        
+        let points = `${x1},${y1} ${x2},${y2} ${x3},${y3} ${x1},${y1}`
+        
+        return dc.drawingObj().polygon(points).fill('red').stroke({ width: 1 })
+    }
+
+    processProgram(program)
+    {
+        this.empty();
+        program.statements.forEach(st => this.processStatement(st))
+        this.moveCursorToContext()
+
+    }
+
+    processStatement(st)
+    {
+        this.processor.processStatement(st,this.context);
+    }
+
+    empty()
+    {
+        this.context.drawingObj().clear();
+        this.context.drawingObj().add(this.cursorObj);
+    }
+
+    moveCursorToContext()
+    {
+        this.cursorObj.center(this.context.lastX,this.context.lastY)
+        let currentCursorAngle = this.cursorObj.transform().rotate;
+        let dAngle = this.context.angle - currentCursorAngle;
+        this.cursorObj.rotate(dAngle);
+    }
+}
+
+module.exports = { Painter}
