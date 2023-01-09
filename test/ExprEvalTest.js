4765e1674623f23daf3a39c371e25d2621707e07
slior <lior.schejter@gmail.com>
2023-01-09 23:11:47 +0200

    Test for expression evaluator

---
+++ test/ExprEvalTest.js
@@ -0,0 +1,72 @@
+const assert = require('assert');
+const rewire = require("rewire");
+const {number,binOp} = require('./util')
+
+const ExprEval = rewire('../src/LogoVM.js').__get__("ExprEval")
+
+describe("Expr Evaluator",function() {
+    it("Should evaluate number literals",function() {
+        let ee = new ExprEval()
+
+        let result = ee.eval(number(5))
+        assert.strictEqual(result,5)
+    })
+
+    it("Should evaluate add expr",function() {
+        let ee = new ExprEval()
+        assert.strictEqual(
+            ee.eval(binOp('+',number(5),number(100)))
+            ,105)
+
+        assert.strictEqual(
+            ee.eval(binOp('+',binOp('+',number(4),number(2)),number(10))),
+            16)
+    })
+
+    it("Should evaluate subtraction expr",function() {
+        let ee = new ExprEval()
+        assert.strictEqual(
+            ee.eval(binOp('-',number(500),number(220)))
+            ,280)
+
+        assert.strictEqual(
+            ee.eval(binOp('-',binOp('-',number(4),number(2)),number(5))),
+            -3)
+    })
+
+    it("Should evaluate multiplication expr",function() {
+        let ee = new ExprEval()
+        assert.strictEqual(
+            ee.eval(binOp('*',number(-1),number(45)))
+            ,-45)
+
+        assert.strictEqual(
+            ee.eval(binOp('*',binOp('*',number(4),number(2)),number(5))),
+            40)
+    })
+
+    it("Should evaluate division expr",function() {
+        let ee = new ExprEval()
+        assert.strictEqual(
+            ee.eval(binOp('/',number(1),number(4)))
+            ,0.25)
+
+        assert.strictEqual(
+            ee.eval(binOp('/',binOp('/',number(4),number(1)),number(2))),
+            2)
+    })
+
+    it("Should evaluate mixed operators",function() {
+        let ee = new ExprEval()
+
+        assert.strictEqual(
+            ee.eval(binOp('*',binOp('/',number(4),number(1)),number(8))),
+            32)
+
+        assert.strictEqual(
+            ee.eval(binOp('*',binOp('+',number(4),number(5)),number(3))),
+            27)
+        
+
+    })
+})
